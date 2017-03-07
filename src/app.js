292ffdf275cf44b66c9870225b3be9bf50427d11
Abhilash Singh <daft300punk@gmail.com>
2017-03-07 22:03:11 +0530

    Update Styles

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(domainMiddleware);
+app.use('/api', express.static('src/public'));
 
 
 const apiRouter = new express.Router();
