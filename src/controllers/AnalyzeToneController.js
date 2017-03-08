fef813762aa6f9d754daf8823a9f92653329d4a3
Abhilash Singh <daft300punk@gmail.com>
2017-03-08 09:39:04 +0530

    Error handling

--- src/controllers/AnalyzeToneController.js
+++ src/controllers/AnalyzeToneController.js
@@ -8,27 +8,33 @@ export default {
 }
 
 async function analyzeTone(req, res) {
-  var availableLang = await TranslateTextService.getIdentifiableLanguages();
+  try {
+    var availableLang = await TranslateTextService.getIdentifiableLanguages();
 
-  var inputString = req.query['inputString'];
-  var destinationLang = req.query['selectedLang'];
-  console.log(destinationLang);
+    var inputString = req.query['inputString'];
+    var destinationLang = req.query['selectedLang'];
+    console.log(destinationLang);
 
-  if (inputString != null) {
-    var translatedString = await TranslateTextService.translateText(inputString, destinationLang);
-    var analyzedToneObject = await AnalyzeToneService.analyzeTone(inputString);
-    var analyzedToneString = "";
-    for (let i = 0; i < 5; i++) {
-      analyzedToneString += `${analyzedToneObject.document_tone.tone_categories[2].tones[i].tone_name} : ${analyzedToneObject.document_tone.tone_categories[2].tones[i].score} \n`
+    if (inputString != null) {
+      var translatedString = await TranslateTextService.translateText(inputString, destinationLang);
+      var analyzedToneObject = await AnalyzeToneService.analyzeTone(inputString);
+      var analyzedToneString = "";
+      for (let i = 0; i < 5; i++) {
+        analyzedToneString += `${analyzedToneObject.document_tone.tone_categories[2].tones[i].tone_name} : ${analyzedToneObject.document_tone.tone_categories[2].tones[i].score} \n`
+      }
+
+      var recordToBeInserted = {
+        inputString: inputString,
+        destinationLang: destinationLang,
+        translatedString: translatedString
+      }
+      console.log("recordInserted", recordToBeInserted);
+      var recordInserted = await InsertHistoryService.insertHistory(recordToBeInserted);
     }
-
-    var recordToBeInserted = {
-      inputString: inputString,
-      destinationLang: destinationLang,
-      translatedString: translatedString
-    }
-    console.log("recordInserted", recordToBeInserted);
-    var recordInserted = await InsertHistoryService.insertHistory(recordToBeInserted);
+    res.render('index', { result: analyzedToneString, availableLangArray: availableLang.languages, translatedText: translatedString });
+  } catch(err) {
+    console.log("Error", err.error_code, err.error_message);
+    let error_msg = `Error Code: ${err.error_code} <br> Error Message: ${err.error_message}`
+    res.send(error_msg);
   }
-  res.render('index', { result: analyzedToneString, availableLangArray: availableLang.languages, translatedText: translatedString });
 }
